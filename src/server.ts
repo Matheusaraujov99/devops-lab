5aa106dc280c1c037bda22315c924f802bb29098
Matheusaraujov99 <matheusaraujov99@gmail.com>
2025-11-04 13:12:17 -0300

    feat:atualização servidor express para estático

--- src/server.ts
+++ src/server.ts
@@ -4,34 +4,38 @@ import sequelize from "./config/database";
 import labRoutes from "./routes/labRoutes";
 import authRoutes from "./routes/authRoutes";
 import setupSwagger from "./swagger";
+import * as path from "path";
 
 const app = express();
 
-// Configurações globais
 app.use(express.json());
 app.use(cors());
 
-// 🧩 Swagger precisa ser configurado ANTES das rotas
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
 setupSwagger(app);
 
-// Rotas principais
 app.use("/auth", authRoutes);
 app.use("/labs", labRoutes);
 
-// Testa a conexão com o banco antes de sincronizar
 sequelize.authenticate()
-  .then(() => {
-    console.log("✅ Conectado ao banco PostgreSQL com sucesso!");
+ .then(() => {
+console.log("✅ Conectado ao banco PostgreSQL com sucesso!");
 
-    // Sincroniza o banco e inicia o servidor
-    return sequelize.sync({ alter: true });
-  })
-  .then(() => {
-    app.listen(3001, () => {
-      console.log("🚀 Servidor rodando em http://localhost:3001");
-      console.log("📘 Documentação Swagger disponível em http://localhost:3001/api-docs");
-    });
-  })
-  .catch((error) => {
-    console.error("❌ Erro ao conectar ou sincronizar o banco:", error);
-  });
+return sequelize.sync({ alter: true });
+})
+.then(() => {
+    const PORT = process.env.PORT || 3001; 
+
+ app.listen(PORT, () => {
+console.log(`🚀 Servidor rodando na porta ${PORT}`);
+ console.log("📘 Documentação Swagger disponível em /api-docs (após o deploy)");
+});
+})
+ .catch((error) => {
+console.error("❌ Erro ao conectar ou sincronizar o banco:", error);
+ });
