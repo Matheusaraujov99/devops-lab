642dca3bc0d23419d1f116227e8cfdbeaa48381c
Matheusaraujov99 <matheusaraujov99@gmail.com>
2025-11-09 11:04:51 -0300

    atualizações no json e server ts

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,14 @@ import authRoutes from "./routes/authRoutes";
 import setupSwagger from "./swagger";
 import * as path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 3001;
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, "../")));
 
-// Rota principal -> abre o index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
@@ -30,8 +28,8 @@ async function startServer() {
     await connectDB();
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
-      console.log(`Documentação Swagger: http://localhost:${PORT}/api-docs`);
-      console.log(`Front-end disponível em: http://localhost:${PORT}`);
+      console.log(`Swagger: http://localhost:${PORT}/api-docs`);
+      console.log(`Front-end: http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("Falha ao iniciar o servidor:", error);
